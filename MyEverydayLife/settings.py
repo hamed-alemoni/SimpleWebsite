78368f7e13b3e82f102f09a75ef9d40d71e8a21a
hamed alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2023-07-21 13:15:30 +0330

    Update settings.py

--- MyEverydayLife/settings.py
+++ MyEverydayLife/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'hamed.alemonie@gmail.com'
-EMAIL_HOST_PASSWORD = 'ocqkctowvccckytb'
+EMAIL_HOST_USER = 'YOUR EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR PASSWORD'
